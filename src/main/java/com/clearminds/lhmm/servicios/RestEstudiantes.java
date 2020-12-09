75834f8efa22a331599ecc1f59fb2e943b3232f3
luis <luishm2024@gmail.com>
2020-12-09 12:36:11 -0500

    rest funcionando

---
+++ src/main/java/com/clearminds/lhmm/servicios/RestEstudiantes.java
@@ -0,0 +1,45 @@
+package com.clearminds.lhmm.servicios;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.clearminds.entidades.Persona;
+import com.clearminds.lhmm.dtos.Estudiante;
+
+@Path("/estudiantes")
+public class RestEstudiantes {
+	
+	@Path("/insertar")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void insertar(Estudiante estudiante){
+		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
+		try {
+			srvEstudiante.insertarEstudiante(estudiante);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		System.out.println(estudiante);
+	}
+	
+	@Path("/actualizar")
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void actualizar(Estudiante estudiante) {
+		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
+		try {
+			srvEstudiante.actualizarEstudiante(estudiante);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		System.out.println(estudiante);
+	}
+
+}
